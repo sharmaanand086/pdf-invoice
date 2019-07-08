2d2a83909f6c510cfedc1ebe7e1ab4f3792bd127
sharmaanand086 <38725515+sharmaanand086@users.noreply.github.com>
2019-07-08 17:43:55 +0530

    Update conn.cfg.php

--- conn.cfg.php
+++ conn.cfg.php
@@ -1,5 +1,9 @@
-<?php
-
-$connInfo = array('connectionName:ad129:i:e76b428d25a0c1c740b868a66323a2cc:"This is the connection for application.infusionsoft.com"');
-
+<?php
+
+
+
+$connInfo = array('connectionName:ad129:i:ehdhgfdhgfdhdhfdhdc:"This is the connection for application.infusionsoft.com"');
+
+
+
 ?>
